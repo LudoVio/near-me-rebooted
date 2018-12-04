ce593b659d547c4ae3558a0e47df978d3fd239bd
Ludovic Viaud <ludovic.viaud@gmail.com>
2018-12-04 16:44:10 +0100

    Handle fetching and storing venues with vuex

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import VenueService from "@/services/VenueService";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    venues: []
   },
   mutations: {
-
+    setVenues(state, venues) {
+      state.venues = venues;
+    }
   },
   actions: {
-
+    fetchVenues({ commit }) {
+      VenueService.getVenues(
+        venues => {
+          commit("setVenues", venues);
+        },
+        error => {
+          console.log("There was an error: ", error); 
+        }
+      );
+    }
   }
 })
