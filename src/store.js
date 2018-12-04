f0a7875faab282e7b7c045a58fb221f2a01f5a03
Ludovic Viaud <ludovic.viaud@gmail.com>
2018-12-04 19:03:10 +0100

    Install and use bootstrap and fontawesome

--- src/store.js
+++ src/store.js
@@ -7,20 +7,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    venues: []
+    venues: [],
+    loading: true
   },
   mutations: {
     setVenues(state, venues) {
       state.venues = venues;
+    },
+    setLoading(state, value) {
+      state.loading = value;
     }
   },
   actions: {
     fetchVenues({ commit }) {
+      commit("setLoading", true);
       VenueService.getVenues(
         venues => {
           commit("setVenues", venues);
+          commit("setLoading", false);
         },
         error => {
+          commit("setVenues", []);
+          commit("setLoading", false);
           console.log("There was an error: ", error); 
         }
       );
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="loading" class="loader">
+      <fa icon="sync" class="fa-spin fa-10x" />
+    </div>
+
+    <div v-else class="card-columns">
+      <div class="card" v-for="venue in venues" :key="venue.id">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">{{ venue }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
+  },
+  methods: mapActions(["fetchVenues"]),
+  computed: mapState(["venues", "loading"]),
+  created() {
+    this.fetchVenues();
   }
 }
 </script>
+
+<style lang="scss">
+  @import "../../node_modules/bootstrap/scss/functions";
+  @import "../../node_modules/bootstrap/scss/variables";
+  @import "../../node_modules/bootstrap/scss/mixins";
+
+  .loader {
+    text-align: center;
+    padding-top: 30px;
+  }
+
+  .card-columns {
+    @include media-breakpoint-only(sm) {
+      column-count: 1;
+    }
+    @include media-breakpoint-only(md) {
+      column-count: 2;
+    }
+    @include media-breakpoint-only(lg) {
+      column-count: 3;
+    }
+  }
+</style>
